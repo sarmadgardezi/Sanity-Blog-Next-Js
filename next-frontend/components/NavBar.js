16d6467f22e7d70b51732ab6754a94fadffd71b4
Sarmad Gardezi <mail@sarmadgardezi.com>
2023-06-30 10:31:07 +0500

    link updated

--- next-frontend/components/NavBar.js
+++ next-frontend/components/NavBar.js
@@ -15,69 +15,7 @@ const NavBar = ({profile}) => {
             <div className="hidden lg:block">
               <ul className="flex items-center">
 
-                <li className="group pl-6">
-
-                  <a href="/#about"
-                    className="cursor-pointe pt-0.5 font-header font-semibold uppercase text-white">About</a>
-
-                  <span className="block h-0.5 w-full bg-transparent group-hover:bg-yellow"></span>
-                </li>
-
-                <li className="group pl-6">
-
-                  <a href="/#services"
-                    className="cursor-pointe pt-0.5 font-header font-semibold uppercase text-white">Services</a>
-
-                  <span className="block h-0.5 w-full bg-transparent group-hover:bg-yellow"></span>
-                </li>
-
-                <li className="group pl-6">
-
-                  <a href="/#portfolio"
-                    className="cursor-pointe pt-0.5 font-header font-semibold uppercase text-white">Portfolio</a>
-
-                  <span className="block h-0.5 w-full bg-transparent group-hover:bg-yellow"></span>
-                </li>
-
-                <li className="group pl-6">
-
-                  <a href="/#clients"
-                    className="cursor-pointe pt-0.5 font-header font-semibold uppercase text-white">Clients</a>
-
-                  <span className="block h-0.5 w-full bg-transparent group-hover:bg-yellow"></span>
-                </li>
-
-                <li className="group pl-6">
-
-                  <a href="/#work"
-                    className="cursor-pointe pt-0.5 font-header font-semibold uppercase text-white">Work</a>
-
-                  <span className="block h-0.5 w-full bg-transparent group-hover:bg-yellow"></span>
-                </li>
-
-                <li className="group pl-6">
-
-                  <a href="/#statistics"
-                    className="cursor-pointe pt-0.5 font-header font-semibold uppercase text-white">Statistics</a>
-
-                  <span className="block h-0.5 w-full bg-transparent group-hover:bg-yellow"></span>
-                </li>
-
-                <li className="group pl-6">
-
-                  <a href="/#blog"
-                    className="cursor-pointe pt-0.5 font-header font-semibold uppercase text-white">Blog</a>
-
-                  <span className="block h-0.5 w-full bg-transparent group-hover:bg-yellow"></span>
-                </li>
-
-                <li className="group pl-6">
-
-                  <a href="/#contact"
-                    className="cursor-pointe pt-0.5 font-header font-semibold uppercase text-white">Contact</a>
-
-                  <span className="block h-0.5 w-full bg-transparent group-hover:bg-yellow"></span>
-                </li>
+               
 
               </ul>
             </div>
